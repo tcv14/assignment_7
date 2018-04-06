12374eca176d1241c17802a22f42a9d676e4886d
Tiffany Cheng <tycheng@bu.edu>
2018-04-05 22:59:49 -0400

    added sentiment analysis with graphs for joy, anger, and anticipation

--- president_speeches.R
+++ president_speeches.R
@@ -68,3 +68,83 @@ carter_word_counts %>%
        x = NULL) +
   ggtitle("Carter Speech") +
   coord_flip()
+
+# for sentiment analysis with nrc
+nrc_joy <- get_sentiments("nrc") %>% 
+  filter(sentiment == "joy")
+nrc_anger <- get_sentiments("nrc") %>% 
+  filter(sentiment == "anger")
+nrc_anticipation <- get_sentiments("nrc") %>% 
+  filter(sentiment == "anticipation")
+
+# bush sentiment analysis with nrc
+bush_joy <- tidy_bush %>%
+  inner_join(nrc_joy) %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 3) %>%
+  mutate(word = reorder(word, n))
+bush_anger <- tidy_bush %>%
+  inner_join(nrc_anger) %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 2) %>%
+  mutate(word = reorder(word, n))
+bush_anticipation <- tidy_bush %>%
+  inner_join(nrc_anticipation) %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 3) %>%
+  mutate(word = reorder(word, n))
+
+gridExtra::grid.arrange(
+ggplot(bush_joy, aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip() +
+  ggtitle("Bush Joy"),
+ggplot(bush_anger, aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip() +
+  ggtitle("Bush Anger"),
+ggplot(bush_anticipation, aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip() +
+  ggtitle("Bush Anticipation"),
+nrow=1
+)
+
+# carter sentiment analysis with nrc
+carter_joy <- tidy_carter %>%
+  inner_join(nrc_joy) %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 2) %>%
+  mutate(word = reorder(word, n))
+carter_anger <- tidy_carter %>%
+  inner_join(nrc_anger) %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 1) %>%
+  mutate(word = reorder(word, n))
+carter_anticipation <- tidy_carter %>%
+  inner_join(nrc_anticipation) %>%
+  count(word, sort = TRUE) %>%
+  filter(n > 2) %>%
+  mutate(word = reorder(word, n))
+
+gridExtra::grid.arrange(
+ggplot(carter_joy, aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip() +
+  ggtitle("Carter Joy"),
+ggplot(carter_anger, aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip() +
+  ggtitle("Carter Anger"),
+ggplot(carter_anticipation, aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip() +
+  ggtitle("Carter Anticipation"),
+nrow=1
+)
